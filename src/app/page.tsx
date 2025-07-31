ba23cf9d0959667dcd4c27a7aec63655779dd420
Mateus Werneck <mateuswerneckelizeu@yahoo.com.br>
2025-07-31 02:11:50 -0300

    Add initial project files and MagicUI components
    Added MIT license, project configuration, and initial dependencies. Introduced several MagicUI components and utility functions. Updated global styles and main layout/page files to integrate new components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,59 @@
-import Image from "next/image";
+import { BlurFade } from '@/components/magicui/blur-fade';
+import { DotPattern } from '@/components/magicui/dot-pattern';
+import { cn } from '@/lib/utils';
+import { GridPattern } from '@/components/magicui/grid-pattern';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="relative flex flex-col justify-between min-h-screen bg-neutral-900 overflow-hidden">
+      <main className="relative text-center *:text-white flex flex-col items-center justify-center flex-1">
+        <BlurFade>
+          <p className="font-bold text-3xl text-white">👋 Fala chefe, de boa?</p>
+        </BlurFade>
+        <section className="mt-4 text-2xl text-white">
+          <pre>
+            {String.raw`
+/\___/\
+/       \
+| o w o |
+\_______/
+/w w\
+\w w/
+S
+`}
+          </pre>
+        </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <BlurFade className="mt-8" direction="up">
+          <div className="mt-8 *:text-white font-black">
+            <ul className="flex flex-row items-center justify-center space-x-8">
+              <li className="text-lg hover:transform hover:scale-105 transition-transform duration-300">
+                <a href="https://github.com/Mwrnk" target="_blank" rel="noopener noreferrer">
+                  GitHub
+                </a>
+              </li>
+              <li className="text-lg hover:transform hover:scale-105 transition-transform duration-300">
+                <a
+                  href="https://www.linkedin.com/in/mateuswerneck/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
+        </BlurFade>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <GridPattern
+        width={50}
+        height={50}
+        x={5}
+        y={520}
+        className={cn('[mask-image:linear-gradient(to_top_right,white,transparent,transparent)] ')}
+      />
+      <footer className="text-center pt-4 z-10 relative">
+        <p className="font-mono text-sm text-gray-300">© 2025 Mateus Werneck 💻</p>
       </footer>
     </div>
   );
